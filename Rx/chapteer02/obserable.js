c84128e1b4712213b58e53bfdfc0f5de75f7906c
ZephyrJS <zhengzefanyy@gmail.com>
2020-03-19 21:40:23 +0800

    Observer and Observer

---
+++ Rx/chapteer02/obserable.js
@@ -0,0 +1,12 @@
+// import { Observable } from 'rxjs'
+const { Observable } = require('rxjs')
+const onSubscribe = observer => {
+    observer.next(1)
+    observer.next(2)
+    observer.next(3)
+}
+const source$ = new Observable(onSubscribe)
+const theObserver = {
+    next: item => console.log(item)
+}
+source$.subscribe(theObserver)
